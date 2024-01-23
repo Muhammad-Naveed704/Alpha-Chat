a22158ffb1a09ac4bdd42bb6055b8194660d20be
Muhammad-Naveed704 <107260698+Muhammad-Naveed704@users.noreply.github.com>
2024-01-24 02:46:15 +0500

    Update firebaseConfig.js

--- firebaseConfig.js
+++ firebaseConfig.js
@@ -20,13 +20,7 @@ import {
 } from "https://www.gstatic.com/firebasejs/10.0.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDYTfVZVUClyYk7pSEDpq2R_oc6uHt7DPQ",
-  authDomain: "alpha-chat-7e3da.firebaseapp.com",
-  projectId: "alpha-chat-7e3da",
-  storageBucket: "alpha-chat-7e3da.appspot.com",
-  messagingSenderId: "715921803252",
-  appId: "1:715921803252:web:35731fce493816f8e99ba1",
-  measurementId: "G-9NJYPPXXZY",
+ 
 };
 
 const app = initializeApp(firebaseConfig);
